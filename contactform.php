a8e610afc9a349dcecbdcabe4bbb7cc4cc82bbd4
robdestiny <36769287+robdestiny@users.noreply.github.com>
2018-02-25 17:49:54 +0100

    fix?

--- contactform.php
+++ contactform.php
@@ -2,7 +2,7 @@
 
 if (isset($_POST['submit'])) {
   $name = $_POST['name'];
-  $mailFrom = $_POST['mail'];
+  $mailFrom = $_POST['email'];
   $message = $_POST['message'];
 
   $mailTo = "robertdownsfreelance@gmail.com";
