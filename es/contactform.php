7c29cad6c06a47e141ebd46b0830dddcfba477b5
robdestiny <36769287+robdestiny@users.noreply.github.com>
2018-02-27 09:40:49 +0100

    trying it

---
+++ es/contactform.php
@@ -0,0 +1,14 @@
+<?php
+
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $mailFrom = $_POST['email'];
+  $message = $_POST['message'];
+
+  $mailTo = "robertdownsfreelance@gmail.com";
+  $headers = "From: ".$mailFrom;
+  $txt = "You have received an email from ".$name.".\n\n".$message;
+
+  mail($mailTo, $subject, $txt, $headers);
+  header ("Location: index.php?mailsend")
+}
